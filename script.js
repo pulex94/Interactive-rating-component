f2b3f3e30058c56f9ab5bccba710b0accf80f32a
Alex Lazzarotto <146884321+pulex94@users.noreply.github.com>
2023-12-29 16:40:35 +0100

    Ended Project

---
+++ script.js
@@ -0,0 +1,30 @@
+let ratingScore;
+document.addEventListener("DOMContentLoaded", () => {
+  const buttons = document.querySelectorAll(".btn-value");
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      ratingScore = e.target.value;
+    });
+  });
+});
+
+const rating = document.getElementById("rating");
+const p = document.createElement("p");
+
+/* ------------------ */
+const confirmBtn = document.getElementById("confirm-btn");
+const continueBtn = document.getElementById("continue-btn");
+const container = document.querySelector(".container");
+const thankYou = document.querySelector(".thank-you");
+/* ------------------ */
+confirmBtn.addEventListener("click", () => {
+  p.innerHTML = "You selected " + ratingScore + " out of 5";
+  rating.appendChild(p);
+  thankYou.style.display = "flex";
+  container.style.display = "none";
+});
+continueBtn.addEventListener("click", () => {
+  thankYou.style.display = "none";
+  container.style.display = "flex";
+});
